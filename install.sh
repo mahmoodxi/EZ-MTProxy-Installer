bd7441006d1096d4d2570da83f51efaadb191a2a
mahmoodxi <36129031+mahmoodxi@users.noreply.github.com>
2020-12-12 16:11:41 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 apt update
-apt install python3-pip -y
+apt install python3-pip git nload -y
+wget https://github.com/mahmoodxi/EZ-MTProxy-Installer/raw/main/official-mtp.py
 python3 official-mtp.py
